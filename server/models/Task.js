151d0c5c6ed4b5bd5853965c0721a8c072ef982c
Mtxilay <mtxilayf@gmail.com>
2025-06-27 13:17:06 +0300

    Completed

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,10 +1,16 @@
-// Require mongoose
+const mongoose=require('mongoose');
 
+const taskSchema = new mongoose.Schema({
+  title: {
+    type : String,
+    required: true
+  },
+  done: {
+    type: Boolean,
+    default: false
+  },  
+});
 
-// Implement schema for a task
-// Each task should have:
-// - title (String, required)
-// - done (Boolean, defaults to false)
+const Task = mongoose.model('Task', taskSchema);
 
-
-// Export the model as 'Task'
+module.exports = Task;
--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const apiRoutes = require('./routes/api');
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to MongoDB"))
